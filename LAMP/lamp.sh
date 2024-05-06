5ba5e8b15a03fd29e6320d785e3177c1ad954e70
citadelict <chidiaustin78@gmail.com>
2024-05-06 05:37:06 -0700

    Update lamp.sh

--- LAMP/lamp.sh
+++ LAMP/lamp.sh
@@ -1,1 +1,34 @@
+# update Ec2 instance  and install apache
 
+sudo apt update
+sudo install apache2
+
+sudo systemctl start apache2
+sudo systemctl enable apache2
+
+# Install MySQL
+
+
+sudo apt install mysql-server
+sudo systemctl start mysql
+sudo mysql_secure_installation
+
+
+# Install PHP
+
+
+sudo apt install php libapache2-mod-php php-mysql
+sudo systemctl restart apache2
+
+
+# Create a PHP info file
+#use vim or nano editor
+
+sudo nano /var/www/chidiaustin/info.php
+
+#write into your new php file
+
+<?php phpinfo();
+
+
+#visit the ip address/info.php
